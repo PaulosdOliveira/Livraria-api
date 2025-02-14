4e214fd291e4ef4dba8a32dd2a1c7d6aaba4e4b4
PaulosdOliveira <pauloricardomilos@gmail.com>
2025-02-14 17:45:21 -0300

    Criação do projeto

---
+++ livraria-api/src/main/java/com/io/github/paulodsoliveira/livraria_api/LivrariaApiApplication.java
@@ -0,0 +1,13 @@
+package com.io.github.paulodsoliveira.livraria_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaApiApplication.class, args);
+	}
+
+}
---
+++ livraria-api/src/test/java/com/io/github/paulodsoliveira/livraria_api/LivrariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.github.paulodsoliveira.livraria_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
