890510633f30b5fc356909f741f30160ffa3db6b
PaulosdOliveira <pauloricardomilos@gmail.com>
2025-02-22 13:49:55 -0300

    Corrigindo validação de livro

--- src/main/java/io/github/paulosdoliveira/livrariaapi/validations/LivroValidator.java
+++ src/main/java/io/github/paulosdoliveira/livrariaapi/validations/LivroValidator.java
@@ -22,10 +22,10 @@ public class LivroValidator {
     private AutorService autorService;
 
     public Autor validar(Livro livro, UUID idAutor) {
-        if (idAutor == null) {
+        if (isbnExiste(livro)) lancarErro("Este ISBN já está cadastrado");
+        if (tituloExiste(livro)) lancarErro("Já existe um livro cadastrado com esse titulo");
+        if (idAutor != null) {
             var autor = autorService.buscarAutorAtivoPorId(idAutor);
-            if (isbnExiste(livro)) lancarErro("Este ISBN já está cadastrado");
-            if (tituloExiste(livro)) lancarErro("Já existe um livro cadastrado com esse titulo");
             if (autor == null) throw new AutorInativoException("Autor  não existe");
             livro.setAutor(autor);
             return autor;
