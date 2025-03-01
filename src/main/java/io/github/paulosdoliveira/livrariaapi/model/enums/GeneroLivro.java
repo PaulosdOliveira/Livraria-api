dc57b5f0140fff821781191914aef111c0964622
PaulosdOliveira <pauloricardomilos@gmail.com>
2025-03-01 15:27:03 -0300

    Corrigindo método valueOf

--- src/main/java/io/github/paulosdoliveira/livrariaapi/model/enums/GeneroLivro.java
+++ src/main/java/io/github/paulosdoliveira/livrariaapi/model/enums/GeneroLivro.java
@@ -3,19 +3,26 @@ package io.github.paulosdoliveira.livrariaapi.model.enums;
 import java.util.Arrays;
 
 public enum GeneroLivro {
-    DRAMA("Drama"),
+    DRAMA("DRAMA"),
     ROMANCE("ROMANCE"),
     CIENCIA("CIENCIA"),
     TERROR("TERROR"),
     COMEDIA("COMEDIA"),
     SUSPENSE("SUSPENSE");
 
+    private final String genero;
+
     GeneroLivro(String genero) {
+        this.genero = genero;
+    }
+
+    public String getGenero(){
+        return this.genero;
     }
     
     public static GeneroLivro valueOfString(String genero){
         GeneroLivro generoLivro = Arrays.stream(values())
-                .filter(String -> String.equals(genero))
+                .filter( livro -> livro.genero.equals(genero.toUpperCase()))
                 .findFirst()
                 .orElse(null);
         return generoLivro;
